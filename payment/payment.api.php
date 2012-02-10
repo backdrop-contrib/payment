94a461eaac4a84cab5979e7d7afafba6376d4c89
Bart Feenstra <bart@mynameisbart.com>
2012-02-10 13:33:30 +0100

    Allow payment method controller classes to be overridden in hook_payment_method_controller_info().

--- payment/payment.api.php
+++ payment/payment.api.php
@@ -38,7 +38,12 @@ function hook_payment_status_info_alter(&$statuses_info) {
  * Defines payment method controllers.
  *
  * @return array
- *   An array with the names of payment method controller classes.
+ *   An array with the names of payment method controller classes. Keys may
+ *   either specific aliases for their values (the classes), or be numeric
+ *   (left empty) to make them default to the controller class names they
+ *   belong to. This allows hook_payment_method_controller_info_alter() to
+ *   override payment method controller class by setting a different class name
+ *   for an alias.
  */
 function hook_payment_method_controller_info() {
   return array(
@@ -51,13 +56,15 @@ function hook_payment_method_controller_info() {
  * Alters payment method controllers.
  *
  * @param array
- *   An array with the names of payment method controller classes.
+ *   Keys are payment controller aliases, values are actual payment method
+ *   controller class names.
  */
 function hook_payment_method_controller_info_alter(&$controllers_info) {
   // Remvove a payment method controller.
-  $reversed = array_reverse($controllers_info);
-  unset($reversed['FooPaymentMethodController']);
-  $controllers_info = array_reverse($reversed);
+  unset($controllers_info['FooPaymentMethodController']);
+
+  // Replace PaymentMethodControllerUnavailable with another controller.
+  $controllers_info['PaymentMethodControllerUnavailable'] = 'FooPaymentMethodControllerUnavailableAdvanced';
 }
 
 /**
