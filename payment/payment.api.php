4bb5505b3d5f1549f8f747a666d217ce4a84e96a
Bart Feenstra <bart@mynameisbart.com>
2012-02-19 18:11:55 +0100

    Fixed #1370128: Payment status log.

--- payment/payment.api.php
+++ payment/payment.api.php
@@ -9,11 +9,11 @@
  * Defines payment statuses.
  *
  * @return array
- *   An array with PaymentStatusInfo objects.
+ *   An array with PaymentStatus objects.
  */
 function hook_payment_status_info() {
   return array(
-    new PaymentStatusInfo(array(
+    new PaymentStatus(array(
       'description' => t('Foo payments are still being processed by Bar to guarantee their authenticity.'),
       'status' => PAYMENT_STATUS_FOO,
       'parents' => array(PAYMENT_STATUS_PENDING),
@@ -26,7 +26,7 @@ function hook_payment_status_info() {
  * Alters payment statuses.
  *
  * @param $statuses_info array
- *   An array with PaymentStatusInfo objects.
+ *   An array with PaymentStatus objects.
  *
  * @return NULL
  */
