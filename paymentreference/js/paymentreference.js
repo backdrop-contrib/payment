f3cef9c82559c810339893b41b115f5b0fdc2b26
Bart Feenstra <bart@mynameisbart.com>
2012-04-12 12:13:52 +0200

    Fixed #1524556: Payment reference field.

---
+++ paymentreference/js/paymentreference.js
@@ -0,0 +1,42 @@
+(function($) {
+  /**
+   * Refresh this window's opener's payment references.
+   */
+  $(document).ready(function() {
+    regex = new RegExp('^' + Drupal.settings.basePath + 'paymentreference/finish');
+    if (regex.test(window.location.pathname) && window.opener) {
+      window.opener.Drupal.PaymentreferenceRefreshButtons();
+    }
+  });
+
+  /**
+   * Convert "close this window" messages to links.
+   */
+  Drupal.behaviors.PaymentreferenceWindowCloseLink = {
+    attach: function(context) {
+      $('span.paymentreference-window-close').each(function() {
+        $(this).replaceWith('<a href="#" class="paymentreference-window-close">' + this.innerHTML + '</a>');
+      });
+      $('a.paymentreference-window-close').bind('click', function() {
+        window.opener.focus();
+        window.close();
+      });
+    }
+  }
+
+  /**
+   * Refresh all payment references.
+   */
+  Drupal.PaymentreferenceRefreshButtons = function() {
+    $('.paymentreference-refresh-button').each(function() {
+      if (!Drupal.settings.PaymentreferencePaymentAvailable[Drupal.settings.ajax[this.id].wrapper]) {
+        $(this).trigger('mousedown');
+      }
+    });
+  }
+
+  /**
+   * Set an interval to refresh all payment references.
+   */
+  setInterval(Drupal.PaymentreferenceRefreshButtons, 30000);
+})(jQuery);
